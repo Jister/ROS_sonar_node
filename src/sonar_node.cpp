ae809fa9b1920cb71a9ee929a720bdd7d052dbd0
Jister <chenjiesjtu14@gmail.com>
2015-12-13 10:37:47 +0800

    update sonar serial

--- src/sonar_node.cpp
+++ src/sonar_node.cpp
@@ -2,359 +2,120 @@
 #include <stdlib.h>
 #include <math.h>
 #include <string.h>
+#include <errno.h>
 #include <wiringPi.h>
+#include <wiringSerial.h>
 #include "ros/ros.h"
 #include "sonar/Sonar_raw.h"
 
-#define Sonar_filter 	100
+#define SERIAL_PORT "/dev/ttyAMA0"
+#define MAXSIZE 60
 
-int sonar1_start_time;
-int sonar1_stop_time;
-int sonar1_time;
-int sonar2_start_time;
-int sonar2_stop_time;
-int sonar2_time;
-int sonar3_start_time;
-int sonar3_stop_time;
-int sonar3_time;
-int sonar4_start_time;
-int sonar4_stop_time;
-int sonar4_time;
-int sonar5_start_time;
-int sonar5_stop_time;
-int sonar5_time;
-int sonar6_start_time;
-int sonar6_stop_time;
-int sonar6_time;
-int sonar7_start_time;
-int sonar7_stop_time;
-int sonar7_time;
-sonar::Sonar_raw sonar_raw;      //当前声纳数据      
-sonar::Sonar_raw sonar_prev;     //前一次声纳数据
-sonar::Sonar_raw sonar_pprev;    //上上次的声纳数据
-sonar::Sonar_raw sonar_filtered; //求平均后声纳数据
+sonar::Sonar_raw sonar_raw;
+/*ringbuffer*/  
+char ringbuf[MAXSIZE];
+char readbuf[18];
+unsigned short  data[9];
+int read_addr = 0;  
+int write_addr = 0;  
+unsigned short crc_data= 0;
 
-void Interrupt1()             //声纳1中断
+int next_data_handle(int addr)     
+{     
+  return (addr + 1) == MAXSIZE ?  0 : (addr + 1) ;     
+}     
+
+void write_data(char data)  
+{  
+  *(ringbuf+write_addr) = data;  
+  write_addr = next_data_handle(write_addr);  
+}  
+
+/*crc*/
+unsigned short crc_update(unsigned short  crc ,  unsigned char data)
 {
-  if(digitalRead(0) == 1)
-  {
-    sonar1_start_time = micros();    //上升沿触发时，记下时间
-  }
-  if(digitalRead(0) == 0)            //下降沿触发时，记下时间
-  {
-    sonar1_stop_time = micros();
-    digitalWrite(21,LOW);            //关闭声纳1,开启声纳2
-    digitalWrite(22,HIGH);
-  }
+  data ^= (crc & 0xff) ;
+  data ^= data << 4;
+  return ((((unsigned short)data << 8) | ((crc >> 8) & 0xff) )^ (unsigned char)(data >> 4) ^ ((unsigned short)data << 3)) ;
 }
 
-void Interrupt2()
+unsigned short crc(void* data, unsigned short count)
 {
-  if(digitalRead(1) == 1)
+  unsigned short crc = 0xff;
+  unsigned char *ptr = (unsigned char*)data;
+  for(int i = 0 ; i < count ; i++)
   {
-    sonar2_start_time = micros();
+    crc = crc_update(crc , *ptr);
+    ptr++;
   }
-  if(digitalRead(1) == 0)
-  {
-    sonar2_stop_time = micros();
-    digitalWrite(22,LOW);
-    digitalWrite(23,HIGH);
-  }
-}
-
-void Interrupt3()
-{
-  if(digitalRead(2) == 1)
-  {
-    sonar3_start_time = micros();
-  }
-  if(digitalRead(2) == 0)
-  {
-    sonar3_stop_time = micros();
-    digitalWrite(23,LOW);
-    digitalWrite(24,HIGH);
-  }
-}
-
-void Interrupt4()
-{
-  if(digitalRead(3) == 1)
-  {
-    sonar4_start_time = micros();
-  }
-  if(digitalRead(3) == 0)
-  {
-    sonar4_stop_time = micros();
-    digitalWrite(24,LOW);
-    digitalWrite(25,HIGH);
-  }
-}
-
-void Interrupt5()
-{
-  if(digitalRead(4) == 1)
-  {
-    sonar5_start_time = micros();
-  }
-  if(digitalRead(4) == 0)
-  {
-    sonar5_stop_time = micros();
-    digitalWrite(25,LOW);
-    digitalWrite(21,HIGH);
-  }
-}
-
-void Interrupt6()
-{
-  if(digitalRead(5) == 1)
-  {
-    sonar6_start_time = micros();
-  }
-  if(digitalRead(5) == 0)
-  {
-    sonar6_stop_time = micros();
-    digitalWrite(26,LOW);
-    digitalWrite(27,HIGH);
-  }
-}
-
-void Interrupt7()
-{
-  if(digitalRead(6) == 1)
-  {
-    sonar7_start_time = micros();
-  }
-  if(digitalRead(6) == 0)
-  {
-    sonar7_stop_time = micros();
-    digitalWrite(27,LOW);
-    digitalWrite(21,HIGH);
-  }
+  return crc;
 }
 
 int main(int argc, char **argv)
 {
   ros::init(argc, argv, "sonar_node");
   ros::NodeHandle n;
-  ros::Publisher chatter_pub = n.advertise<sonar::Sonar_raw>("sonar_data", 1000);
+  ros::Publisher sonar_pub = n.advertise<sonar::Sonar_raw>("sonar_data", 1000);
   ros::Rate loop_rate(25);
-  float distance1 = 0;
-  float distance2 = 0;   
-  float distance3 = 0;   
-  float distance4 = 0;   
-  float distance5 = 0;   
-  float distance6 = 0;
-  float distance7 = 0;
-  int count1=0;
-  int count2=0;
-  int count3=0;
-  int count4=0;
-  int count5=0;
-  int count6=0;
-  int count7=0;
-  wiringPiSetup();
-  pinMode(21, OUTPUT);               //设置引脚为输出模式
-  pinMode(22, OUTPUT);
-  pinMode(23, OUTPUT);
-  pinMode(24, OUTPUT);
-  pinMode(25, OUTPUT);
-  pinMode(26, OUTPUT);
-  pinMode(27, OUTPUT);
-  digitalWrite(21, HIGH);            //开启声纳1
-  digitalWrite(22, LOW);
-  digitalWrite(23, LOW);
-  digitalWrite(24, LOW);
-  digitalWrite(25, LOW);
-  digitalWrite(26, LOW);
-  digitalWrite(27, LOW);
 
-  wiringPiISR(0,INT_EDGE_BOTH,&Interrupt1);    //设置中断，为上升沿下降沿都触发
-  wiringPiISR(1,INT_EDGE_BOTH,&Interrupt2);
-  wiringPiISR(2,INT_EDGE_BOTH,&Interrupt3);
-  wiringPiISR(3,INT_EDGE_BOTH,&Interrupt4);
-  wiringPiISR(4,INT_EDGE_BOTH,&Interrupt5);
-  wiringPiISR(5,INT_EDGE_BOTH,&Interrupt6);
-  wiringPiISR(6,INT_EDGE_BOTH,&Interrupt7);
-
-  while (ros::ok())
+  int fd ;
+/*open the serial*/
+  if ((fd = serialOpen ( SERIAL_PORT, 115200)) < 0)
   {
-    if(sonar1_stop_time > sonar1_start_time)    //计算声纳高电平时间
+    fprintf (stderr, "Unable to open serial device: %s\n", strerror (errno)) ;
+    return 1 ;
+  }
+
+  while(ros::ok())
+  {
+    for(int i = 0 ; i < 29 ; i++)
     {
-      sonar1_time = sonar1_stop_time - sonar1_start_time;
+      write_data(serialGetchar (fd)) ;
     }
-    if(sonar2_stop_time > sonar2_start_time)
+    for(int i = 0 ; i < MAXSIZE ; i++)
     {
-      sonar2_time = sonar2_stop_time - sonar2_start_time;
-    }
-    if(sonar3_stop_time > sonar3_start_time)
-    {
-      sonar3_time = sonar3_stop_time - sonar3_start_time;
-    }
-    if(sonar4_stop_time > sonar4_start_time)
-    {
-      sonar4_time = sonar4_stop_time - sonar4_start_time;
-    }
-    if(sonar5_stop_time > sonar5_start_time)
-    {
-      sonar5_time = sonar5_stop_time - sonar5_start_time;
-    }
-    if(sonar6_stop_time > sonar6_start_time)
-    {
-      sonar6_time = sonar6_stop_time - sonar6_start_time;
-    }
-    if(sonar7_stop_time > sonar7_start_time)
-    {
-      sonar7_time = sonar7_stop_time - sonar7_start_time;
-    }
-    distance1 = sonar1_time/58;                  //计算声纳距离
-    distance2 = sonar2_time/58;
-    distance3 = sonar3_time/58;
-    distance4 = sonar4_time/58;
-    distance5 = sonar5_time/58;
-    distance6 = sonar6_time/58;
-    distance7 = sonar7_time/58;
- 
-    sonar_pprev = sonar_prev;
-    sonar_prev = sonar_raw;
-   
-    sonar_raw.sonar_1 = distance1;
-    if(distance1>600)
-    {
-      sonar_raw.sonar_1 = 600;
-    }
-    if((fabs(distance1-sonar_prev.sonar_1)>Sonar_filter)&&(sonar_prev.sonar_1>0)&&(sonar_pprev.sonar_1>0))
-    {
-      if(count1<3)
+      if((ringbuf[read_addr] == '>') && (ringbuf[next_data_handle(read_addr)] == '*') && (ringbuf[next_data_handle(read_addr)] == '>') 
+      && (ringbuf[next_data_handle(read_addr)] == 0x12) && (ringbuf[next_data_handle(read_addr)] == 0x00) && (ringbuf[next_data_handle(read_addr)] == 'c')  
       {
-        sonar_raw.sonar_1 = sonar_prev.sonar_1;
-        count1++;
+        for(int j = 0 ; j < 18 ; j++)
+        {
+          readbuf[j] = ringbuf[next_data_handle(read_addr)] ; 
+        }
+        break;
       }else
       {
-        sonar_raw.sonar_1 = distance1;
-        count1 = 0;
-      }
-    } 
-
-    sonar_raw.sonar_2 = distance2;
-    if(distance2>600)
-    {
-      sonar_raw.sonar_2 = 600;
-    }
-    if((fabs(distance2-sonar_prev.sonar_2)>Sonar_filter)&&(sonar_prev.sonar_2>0)&&(sonar_pprev.sonar_2>0))
-    {
-      if(count2<3)
-      {
-        sonar_raw.sonar_2 = sonar_prev.sonar_2;
-        count2++;
-      }else
-      {
-        sonar_raw.sonar_2 = distance2;
-        count2 = 0;
-      }
-    } 
-
-    sonar_raw.sonar_3 = distance3;
-    if(distance3>600)
-    {
-      sonar_raw.sonar_3 = 600;
-    }
-    if((fabs(distance3-sonar_prev.sonar_3)>Sonar_filter)&&(sonar_prev.sonar_3>0)&&(sonar_pprev.sonar_3>0))
-    {
-      if(count3<3)
-      {
-        sonar_raw.sonar_3 = sonar_prev.sonar_3;
-        count3++;
-      }else
-      {
-        sonar_raw.sonar_3 = distance3;
-        count3 = 0;
+        next_data_handle(read_addr);
       }
     }
-
-    sonar_raw.sonar_4 = distance4;
-    if(distance4>600)
+    crc_data = (ringbuf[next_data_handle(read_addr)];
+    if( (ringbuf[next_data_handle(read_addr)] == '<') && (ringbuf[next_data_handle(read_addr)] == '#') && (ringbuf[next_data_handle(read_addr)] == '<'))
     {
-      sonar_raw.sonar_4 = 600;
+      data[0] = (readbuf[1]<<8) | readbuf[0] ;
+      data[1] = (readbuf[3]<<8) | readbuf[2] ; 
+      data[2] = (readbuf[5]<<8) | readbuf[4] ; 
+      data[3] = (readbuf[7]<<8) | readbuf[6] ; 
+      data[4] = (readbuf[9]<<8) | readbuf[8] ; 
+      data[5] = (readbuf[11]<<8) | readbuf[10] ; 
+      data[6] = (readbuf[13]<<8) | readbuf[12] ; 
+      data[7] = (readbuf[15]<<8) | readbuf[14] ; 
+      data[8] = (readbuf[17]<<8) | readbuf[16] ;
     }
-    if((fabs(distance4-sonar_prev.sonar_4)>Sonar_filter)&&(sonar_prev.sonar_4>0)&&(sonar_pprev.sonar_4>0))
+    if(crc(data,18) == crc_data)
     {
-      if(count4<3)
-      {
-        sonar_raw.sonar_4 = sonar_prev.sonar_4;
-        count4++;
-      }else
-      {
-        sonar_raw.sonar_4 = distance4;
-        count4 = 0;
-      }
-    }
-
-    sonar_raw.sonar_5 = distance5;
-    if(distance5>600)
-    {
-      sonar_raw.sonar_5 = 600;
-    }
-    if((fabs(distance5-sonar_prev.sonar_5)>Sonar_filter)&&(sonar_prev.sonar_5>0)&&(sonar_pprev.sonar_5>0))
-    {
-      if(count5<3)
-      {
-        sonar_raw.sonar_5 = sonar_prev.sonar_5;
-        count5++;
-      }else
-      {
-        sonar_raw.sonar_5 = distance5;
-        count5 = 0;
-      }
+      sonar_raw.sonar_1 = data[0];
+      sonar_raw.sonar_2 = data[1];
+      sonar_raw.sonar_3 = data[2];
+      sonar_raw.sonar_4 = data[3];
+      sonar_raw.sonar_5 = data[4];
+      sonar_raw.sonar_6 = data[5];
+      sonar_raw.sonar_7 = data[6];
+      sonar_raw.sonar_8 = data[7];
+      sonar_raw.sonar_9 = data[8];
     }
 
-    sonar_raw.sonar_6 = distance6;
-    if(distance6>600)
-    {
-      sonar_raw.sonar_6 = 600;
-    }
-    if((fabs(distance6-sonar_prev.sonar_6)>Sonar_filter)&&(sonar_prev.sonar_6>0)&&(sonar_pprev.sonar_6>0))
-    {
-      if(count6<3)
-      {
-        sonar_raw.sonar_6 = sonar_prev.sonar_6;
-        count6++;
-      }else
-      {
-        sonar_raw.sonar_6 = distance6;
-        count6 = 0;
-      }
-    }
-
-    sonar_raw.sonar_7 = distance7;
-    if(distance7>600)
-    {
-      sonar_raw.sonar_7 = 600;
-    }
-    if((fabs(distance7-sonar_prev.sonar_7)>Sonar_filter)&&(sonar_prev.sonar_7>0)&&(sonar_pprev.sonar_7>0))
-    {
-      if(count7<3)
-      {
-        sonar_raw.sonar_7 = sonar_prev.sonar_7;
-        count7++;
-      }else
-      {
-        sonar_raw.sonar_7 = distance7;
-        count7 = 0;
-      }
-    }
-
-    sonar_filtered.sonar_1 = (int)(sonar_raw.sonar_1 + sonar_prev.sonar_1 + sonar_pprev.sonar_1)/3;    //求平均
-    sonar_filtered.sonar_2 = (int)(sonar_raw.sonar_2 + sonar_prev.sonar_2 + sonar_pprev.sonar_2)/3;
-    sonar_filtered.sonar_3 = (int)(sonar_raw.sonar_3 + sonar_prev.sonar_3 + sonar_pprev.sonar_3)/3;
-    sonar_filtered.sonar_4 = (int)(sonar_raw.sonar_4 + sonar_prev.sonar_4 + sonar_pprev.sonar_4)/3;
-    sonar_filtered.sonar_5 = (int)(sonar_raw.sonar_5 + sonar_prev.sonar_5 + sonar_pprev.sonar_5)/3;
-    sonar_filtered.sonar_6 = (int)(sonar_raw.sonar_6 + sonar_prev.sonar_6 + sonar_pprev.sonar_6)/3;
-    sonar_filtered.sonar_7 = (int)(sonar_raw.sonar_7 + sonar_prev.sonar_7 + sonar_pprev.sonar_7)/3;
-
-    chatter_pub.publish(sonar_filtered);
-  
+    sonar_pub.publish(sonar_raw);
     ros:: spinOnce();
     loop_rate.sleep();
   }
-  return 0;
 }
+
---
+++ src/sonar_node.cpp.cp
@@ -0,0 +1,360 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <string.h>
+#include <wiringPi.h>
+#include "ros/ros.h"
+#include "sonar/Sonar_raw.h"
+
+#define Sonar_filter 	100
+
+int sonar1_start_time;
+int sonar1_stop_time;
+int sonar1_time;
+int sonar2_start_time;
+int sonar2_stop_time;
+int sonar2_time;
+int sonar3_start_time;
+int sonar3_stop_time;
+int sonar3_time;
+int sonar4_start_time;
+int sonar4_stop_time;
+int sonar4_time;
+int sonar5_start_time;
+int sonar5_stop_time;
+int sonar5_time;
+int sonar6_start_time;
+int sonar6_stop_time;
+int sonar6_time;
+int sonar7_start_time;
+int sonar7_stop_time;
+int sonar7_time;
+sonar::Sonar_raw sonar_raw;      //当前声纳数据      
+sonar::Sonar_raw sonar_prev;     //前一次声纳数据
+sonar::Sonar_raw sonar_pprev;    //上上次的声纳数据
+sonar::Sonar_raw sonar_filtered; //求平均后声纳数据
+
+void Interrupt1()             //声纳1中断
+{
+  if(digitalRead(0) == 1)
+  {
+    sonar1_start_time = micros();    //上升沿触发时，记下时间
+  }
+  if(digitalRead(0) == 0)            //下降沿触发时，记下时间
+  {
+    sonar1_stop_time = micros();
+    digitalWrite(21,LOW);            //关闭声纳1,开启声纳2
+    digitalWrite(22,HIGH);
+  }
+}
+
+void Interrupt2()
+{
+  if(digitalRead(1) == 1)
+  {
+    sonar2_start_time = micros();
+  }
+  if(digitalRead(1) == 0)
+  {
+    sonar2_stop_time = micros();
+    digitalWrite(22,LOW);
+    digitalWrite(23,HIGH);
+  }
+}
+
+void Interrupt3()
+{
+  if(digitalRead(2) == 1)
+  {
+    sonar3_start_time = micros();
+  }
+  if(digitalRead(2) == 0)
+  {
+    sonar3_stop_time = micros();
+    digitalWrite(23,LOW);
+    digitalWrite(24,HIGH);
+  }
+}
+
+void Interrupt4()
+{
+  if(digitalRead(3) == 1)
+  {
+    sonar4_start_time = micros();
+  }
+  if(digitalRead(3) == 0)
+  {
+    sonar4_stop_time = micros();
+    digitalWrite(24,LOW);
+    digitalWrite(25,HIGH);
+  }
+}
+
+void Interrupt5()
+{
+  if(digitalRead(4) == 1)
+  {
+    sonar5_start_time = micros();
+  }
+  if(digitalRead(4) == 0)
+  {
+    sonar5_stop_time = micros();
+    digitalWrite(25,LOW);
+    digitalWrite(21,HIGH);
+  }
+}
+
+void Interrupt6()
+{
+  if(digitalRead(5) == 1)
+  {
+    sonar6_start_time = micros();
+  }
+  if(digitalRead(5) == 0)
+  {
+    sonar6_stop_time = micros();
+    digitalWrite(26,LOW);
+    digitalWrite(27,HIGH);
+  }
+}
+
+void Interrupt7()
+{
+  if(digitalRead(6) == 1)
+  {
+    sonar7_start_time = micros();
+  }
+  if(digitalRead(6) == 0)
+  {
+    sonar7_stop_time = micros();
+    digitalWrite(27,LOW);
+    digitalWrite(21,HIGH);
+  }
+}
+
+int main(int argc, char **argv)
+{
+  ros::init(argc, argv, "sonar_node");
+  ros::NodeHandle n;
+  ros::Publisher chatter_pub = n.advertise<sonar::Sonar_raw>("sonar_data", 1000);
+  ros::Rate loop_rate(25);
+  float distance1 = 0;
+  float distance2 = 0;   
+  float distance3 = 0;   
+  float distance4 = 0;   
+  float distance5 = 0;   
+  float distance6 = 0;
+  float distance7 = 0;
+  int count1=0;
+  int count2=0;
+  int count3=0;
+  int count4=0;
+  int count5=0;
+  int count6=0;
+  int count7=0;
+  wiringPiSetup();
+  pinMode(21, OUTPUT);               //设置引脚为输出模式
+  pinMode(22, OUTPUT);
+  pinMode(23, OUTPUT);
+  pinMode(24, OUTPUT);
+  pinMode(25, OUTPUT);
+  pinMode(26, OUTPUT);
+  pinMode(27, OUTPUT);
+  digitalWrite(21, HIGH);            //开启声纳1
+  digitalWrite(22, LOW);
+  digitalWrite(23, LOW);
+  digitalWrite(24, LOW);
+  digitalWrite(25, LOW);
+  digitalWrite(26, LOW);
+  digitalWrite(27, LOW);
+
+  wiringPiISR(0,INT_EDGE_BOTH,&Interrupt1);    //设置中断，为上升沿下降沿都触发
+  wiringPiISR(1,INT_EDGE_BOTH,&Interrupt2);
+  wiringPiISR(2,INT_EDGE_BOTH,&Interrupt3);
+  wiringPiISR(3,INT_EDGE_BOTH,&Interrupt4);
+  wiringPiISR(4,INT_EDGE_BOTH,&Interrupt5);
+  wiringPiISR(5,INT_EDGE_BOTH,&Interrupt6);
+  wiringPiISR(6,INT_EDGE_BOTH,&Interrupt7);
+
+  while (ros::ok())
+  {
+    if(sonar1_stop_time > sonar1_start_time)    //计算声纳高电平时间
+    {
+      sonar1_time = sonar1_stop_time - sonar1_start_time;
+    }
+    if(sonar2_stop_time > sonar2_start_time)
+    {
+      sonar2_time = sonar2_stop_time - sonar2_start_time;
+    }
+    if(sonar3_stop_time > sonar3_start_time)
+    {
+      sonar3_time = sonar3_stop_time - sonar3_start_time;
+    }
+    if(sonar4_stop_time > sonar4_start_time)
+    {
+      sonar4_time = sonar4_stop_time - sonar4_start_time;
+    }
+    if(sonar5_stop_time > sonar5_start_time)
+    {
+      sonar5_time = sonar5_stop_time - sonar5_start_time;
+    }
+    if(sonar6_stop_time > sonar6_start_time)
+    {
+      sonar6_time = sonar6_stop_time - sonar6_start_time;
+    }
+    if(sonar7_stop_time > sonar7_start_time)
+    {
+      sonar7_time = sonar7_stop_time - sonar7_start_time;
+    }
+    distance1 = sonar1_time/58;                  //计算声纳距离
+    distance2 = sonar2_time/58;
+    distance3 = sonar3_time/58;
+    distance4 = sonar4_time/58;
+    distance5 = sonar5_time/58;
+    distance6 = sonar6_time/58;
+    distance7 = sonar7_time/58;
+ 
+    sonar_pprev = sonar_prev;
+    sonar_prev = sonar_raw;
+   
+    sonar_raw.sonar_1 = distance1;
+    if(distance1>600)
+    {
+      sonar_raw.sonar_1 = 600;
+    }
+    if((fabs(distance1-sonar_prev.sonar_1)>Sonar_filter)&&(sonar_prev.sonar_1>0)&&(sonar_pprev.sonar_1>0))
+    {
+      if(count1<3)
+      {
+        sonar_raw.sonar_1 = sonar_prev.sonar_1;
+        count1++;
+      }else
+      {
+        sonar_raw.sonar_1 = distance1;
+        count1 = 0;
+      }
+    } 
+
+    sonar_raw.sonar_2 = distance2;
+    if(distance2>600)
+    {
+      sonar_raw.sonar_2 = 600;
+    }
+    if((fabs(distance2-sonar_prev.sonar_2)>Sonar_filter)&&(sonar_prev.sonar_2>0)&&(sonar_pprev.sonar_2>0))
+    {
+      if(count2<3)
+      {
+        sonar_raw.sonar_2 = sonar_prev.sonar_2;
+        count2++;
+      }else
+      {
+        sonar_raw.sonar_2 = distance2;
+        count2 = 0;
+      }
+    } 
+
+    sonar_raw.sonar_3 = distance3;
+    if(distance3>600)
+    {
+      sonar_raw.sonar_3 = 600;
+    }
+    if((fabs(distance3-sonar_prev.sonar_3)>Sonar_filter)&&(sonar_prev.sonar_3>0)&&(sonar_pprev.sonar_3>0))
+    {
+      if(count3<3)
+      {
+        sonar_raw.sonar_3 = sonar_prev.sonar_3;
+        count3++;
+      }else
+      {
+        sonar_raw.sonar_3 = distance3;
+        count3 = 0;
+      }
+    }
+
+    sonar_raw.sonar_4 = distance4;
+    if(distance4>600)
+    {
+      sonar_raw.sonar_4 = 600;
+    }
+    if((fabs(distance4-sonar_prev.sonar_4)>Sonar_filter)&&(sonar_prev.sonar_4>0)&&(sonar_pprev.sonar_4>0))
+    {
+      if(count4<3)
+      {
+        sonar_raw.sonar_4 = sonar_prev.sonar_4;
+        count4++;
+      }else
+      {
+        sonar_raw.sonar_4 = distance4;
+        count4 = 0;
+      }
+    }
+
+    sonar_raw.sonar_5 = distance5;
+    if(distance5>600)
+    {
+      sonar_raw.sonar_5 = 600;
+    }
+    if((fabs(distance5-sonar_prev.sonar_5)>Sonar_filter)&&(sonar_prev.sonar_5>0)&&(sonar_pprev.sonar_5>0))
+    {
+      if(count5<3)
+      {
+        sonar_raw.sonar_5 = sonar_prev.sonar_5;
+        count5++;
+      }else
+      {
+        sonar_raw.sonar_5 = distance5;
+        count5 = 0;
+      }
+    }
+
+    sonar_raw.sonar_6 = distance6;
+    if(distance6>600)
+    {
+      sonar_raw.sonar_6 = 600;
+    }
+    if((fabs(distance6-sonar_prev.sonar_6)>Sonar_filter)&&(sonar_prev.sonar_6>0)&&(sonar_pprev.sonar_6>0))
+    {
+      if(count6<3)
+      {
+        sonar_raw.sonar_6 = sonar_prev.sonar_6;
+        count6++;
+      }else
+      {
+        sonar_raw.sonar_6 = distance6;
+        count6 = 0;
+      }
+    }
+
+    sonar_raw.sonar_7 = distance7;
+    if(distance7>600)
+    {
+      sonar_raw.sonar_7 = 600;
+    }
+    if((fabs(distance7-sonar_prev.sonar_7)>Sonar_filter)&&(sonar_prev.sonar_7>0)&&(sonar_pprev.sonar_7>0))
+    {
+      if(count7<3)
+      {
+        sonar_raw.sonar_7 = sonar_prev.sonar_7;
+        count7++;
+      }else
+      {
+        sonar_raw.sonar_7 = distance7;
+        count7 = 0;
+      }
+    }
+
+    sonar_filtered.sonar_1 = (int)(sonar_raw.sonar_1 + sonar_prev.sonar_1 + sonar_pprev.sonar_1)/3;    //求平均
+    sonar_filtered.sonar_2 = (int)(sonar_raw.sonar_2 + sonar_prev.sonar_2 + sonar_pprev.sonar_2)/3;
+    sonar_filtered.sonar_3 = (int)(sonar_raw.sonar_3 + sonar_prev.sonar_3 + sonar_pprev.sonar_3)/3;
+    sonar_filtered.sonar_4 = (int)(sonar_raw.sonar_4 + sonar_prev.sonar_4 + sonar_pprev.sonar_4)/3;
+    sonar_filtered.sonar_5 = (int)(sonar_raw.sonar_5 + sonar_prev.sonar_5 + sonar_pprev.sonar_5)/3;
+    sonar_filtered.sonar_6 = (int)(sonar_raw.sonar_6 + sonar_prev.sonar_6 + sonar_pprev.sonar_6)/3;
+    sonar_filtered.sonar_7 = (int)(sonar_raw.sonar_7 + sonar_prev.sonar_7 + sonar_pprev.sonar_7)/3;
+
+    chatter_pub.publish(sonar_filtered);
+  
+    ros:: spinOnce();
+    loop_rate.sleep();
+  }
+  return 0;
+}
